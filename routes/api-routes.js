f289f530745ed258fad57201c51456e633cebe80
Alijan Shiraliyev <aj@Alijans-MacBook-Pro.local>
2018-12-03 21:27:00 -0600

    failureRedirect

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,9 +3,14 @@ const db = require("../models");
 const passport = require("../config/passport");
 
 module.exports = function(app) {
-    app.post("/api/login", passport.authenticate("local"), function(req, res) {
-        res.json("/profile");
-    });
+    app.post(
+        "/api/login",
+        passport.authenticate("local"), // If login info is correct redirect to "profile" page, otherwise return to login page
+        { failureRedirect: "/login" },
+        function(req, res) {
+            res.json("/profile");
+        }
+    );
 
     app.post("/api/signup", function(req, res) {
         db.Agency.create({
