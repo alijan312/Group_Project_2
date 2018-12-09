a40f6e45d70aa165c5b06fb3ba748e1b37dacb60
LealTX <jacob.leal32@gmail.com>
2018-12-09 17:58:46 -0600

    adoption page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,4 +2,8 @@ module.exports = function(app) {
     app.get("/", function(req, res) {
         res.render("index", {});
     });
+
+    app.get("/adoption", function(req, res) {
+        res.render("adoption", {});
+    });
 };
