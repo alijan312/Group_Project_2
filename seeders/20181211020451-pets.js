1c6bd11b25b0e49b491011c63c8ca25b1e48f34f
Alijan Shiraliyev <aj@Alijans-MacBook-Pro.local>
2018-12-10 20:06:49 -0600

    created seed for Agencies table

---
+++ seeders/20181211020451-pets.js
@@ -0,0 +1,66 @@
+"use strict";
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+        return queryInterface.bulkInsert(
+            "Pets",
+            [
+                {
+                    agencyName: "Houston",
+                    agencyPhone: 8127896478,
+                    agencyAddress: "1 Main street",
+                    agencyCity: "Houston",
+                    agencyState: "TX",
+                    agencyZip: 77031,
+                    contactEmail: "houston@houston.com",
+                    createdAt: Sequelize.literal("NOW()"),
+                    updatedAt: Sequelize.literal("NOW()")
+                },
+                {
+                    agencyName: "Austin",
+                    agencyPhone: 8127898374,
+                    agencyAddress: "1 Post Oak street",
+                    agencyCity: "Austin",
+                    agencyState: "TX",
+                    agencyZip: 77645,
+                    contactEmail: "austin@austin.com",
+                    createdAt: Sequelize.literal("NOW()"),
+                    updatedAt: Sequelize.literal("NOW()")
+                },
+                {
+                    agencyName: "Dallas",
+                    agencyPhone: 8127891425,
+                    agencyAddress: "1 Jefferson street",
+                    agencyCity: "Dallas",
+                    agencyState: "TX",
+                    agencyZip: 78354,
+                    contactEmail: "dallas@dallas.com",
+                    createdAt: Sequelize.literal("NOW()"),
+                    updatedAt: Sequelize.literal("NOW()")
+                }
+            ],
+            {}
+        );
+    },
+
+    down: (queryInterface, Sequelize) => {
+        /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+        return queryInterface.bulkDelete("Agencies", null, {});
+    }
+};
